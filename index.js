56f98820de3f7a803c4f80623b20689e241d70d5
olivia <yoon.eunsil@gmail.com>
2024-03-19 16:13:24 -0700

    Add in additional middleware

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use([express.json(), cors()])
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use([express.json(), cors(), requestLogger, unknownEndpoint])
 
 let notes = [
   {

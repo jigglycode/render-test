5577fed7a29f30be2e48224ecefa71a14224e56e
olivia <yoon.eunsil@gmail.com>
2024-03-20 21:05:04 -0700

    Move unknownEndpoint method up

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -108,11 +115,6 @@ app.put('/api/notes/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
-const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-  next()
-}
 
 app.use([unknownEndpoint, errorHandler])
 

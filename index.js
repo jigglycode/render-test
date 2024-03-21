73185dd183c31f7de8bee6de91ae43b689ea0d0b
olivia <yoon.eunsil@gmail.com>
2024-03-20 22:12:38 -0700

    Fix all the routes

--- index.js
+++ index.js
@@ -40,10 +40,6 @@ app.get('/api/notes', (request, response) => {
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -54,26 +50,6 @@ app.post('/api/notes', (request, response, next) => {
     .catch(e => next(e))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => response.json(savedNote))
-})
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
@@ -96,12 +72,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+  const { content, important } = request.body
 
   Note.findByIdAndUpdate(
     request.params.id,

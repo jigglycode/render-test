1f47f291c073767e5d55de96be25a17a861c2ba6
olivia <yoon.eunsil@gmail.com>
2024-03-20 21:26:11 -0700

    Fix it all

--- index.js
+++ index.js
@@ -60,19 +60,19 @@ app.get('/api/notes/:id', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => response.json(savedNote))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -98,19 +98,6 @@ app.delete('/api/notes/:id', (request, response, next) => {
 app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
   const note = {
     content: body.content,
     important: body.important,
@@ -127,7 +114,8 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.use([unknownEndpoint, errorHandler])
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 
